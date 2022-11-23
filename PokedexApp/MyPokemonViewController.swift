b8d064a96ed6b07ad4f9440b7d1a8de63d78aade
Clara Anggraini <claraanggraini_01@yahoo.com>
2022-11-23 14:09:21 +0700

    feat: add my pokemon collection view

--- PokedexApp/MyPokemonViewController.swift
+++ PokedexApp/MyPokemonViewController.swift
@@ -8,27 +8,59 @@
 import UIKit
 
 class MyPokemonViewController: UIViewController {
+
     
-    let nameLbl: UILabel = {
-       let lbl = UILabel()
-        lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.text = "Welcome to My Pokémon!"
-        return lbl
+    lazy var pokemonCV: UICollectionView = {
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
-        self.view.addSubview(nameLbl)
-        NSLayoutConstraint.activate([
-            nameLbl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            nameLbl.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
-        ])
+        self.setupCollectionView()
         
     }
-
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.navigationItem.title = "My Pokemon"
+    }
+    
+    func setupCollectionView(){
+        view.addSubview(pokemonCV)
+        
+        pokemonCV.contentInset = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        NSLayoutConstraint.activate([
+            pokemonCV.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pokemonCV.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pokemonCV.topAnchor.constraint(equalTo: view.topAnchor),
+            pokemonCV.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        pokemonCV.delegate = self
+        pokemonCV.dataSource = self
+        
+        pokemonCV.register(MyPokemonCollectionViewCell.self, forCellWithReuseIdentifier: MyPokemonCollectionViewCell.identifier)
+    }
+}
+
+extension MyPokemonViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 25
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = pokemonCV.dequeueReusableCell(withReuseIdentifier: MyPokemonCollectionViewCell.identifier, for: indexPath) as? MyPokemonCollectionViewCell else {return UICollectionViewCell()}
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
+        
+        let width  = view.frame.width / 2 - 16
+        let height = (view.frame.height)/4 - 32
+        return CGSize(width: width, height: height)
     }
 }
